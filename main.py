e18850ff22ebb430dce17cd1db0ea81c5226df2a
Max Chow <mtchow@usc.edu>
2022-11-29 02:11:11 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# 11/29/2022
+# Created by Max Chow
+
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
