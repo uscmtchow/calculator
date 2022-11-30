08354a3a35990a7cac5c5d8def7c9a58898bf3ea
Max Chow <mtchow@usc.edu>
2022-11-29 17:10:31 -0800

    working calculator

--- main.py
+++ main.py
@@ -3,38 +3,26 @@
 
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.image import Image
 from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
+from kivy.config import Config
+
+Config.set('graphics', 'resizable', 1)
 
 
-class LoginScreen(GridLayout):
-    def __init__(self, **var_args):
-        super(LoginScreen, self).__init__(**var_args)
-
-        self.cols = 2
-        self.add_widget(Label(text='User Name'))
-        self.username = TextInput(multiline=False)
-        self.add_widget(self.username)
-
-        self.add_widget(Label(text='password'))
-        self.password = TextInput(password=True, multiline=False)
-        self.add_widget(self.password)
-
-        # password true is used to hide it
-        # by * self.add_widget(self.password)
-        self.add_widget(Label(text='Comfirm password'))
-        self.password = TextInput(password=True, multiline=False)
-        self.add_widget(self.password)
+# used to solve the equation
+class CalcGridLayout(GridLayout):
+    def calculate(self, calculation):
+        if calculation:
+            try:
+                self.display.text = str(eval(calculation))
+            except Exception:
+                self.display.text = "Error"
 
 
-# the Base Class of our Kivy App
-class MyApp(App):
+class CalculatorApp(App):
     def build(self):
-        # return a LoginScreen() as a root widget
-        return LoginScreen()
+        return CalcGridLayout()
 
-
-if __name__ == '__main__':
-    MyApp().run()
+# runs the code
+calcApp = CalculatorApp()
+calcApp.run()
